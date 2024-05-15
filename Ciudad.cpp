19f0ad8b6425196e83433a3376fa8675df48efea
Jacobogz <jgiraldoz@eafit.edu.co>
2024-05-15 11:30:05 -0500

    Reset

--- Ciudad.cpp
+++
@@ -1,9 +0,0 @@
-#include "Ciudad.h"
-
-std::string Ciudad::obtenerNombre() const {
-    return nombre;
-}
-
-double Ciudad::obtenerTiempo_atencion() const {
-    return tiempo_atencion;
-}
--- Peso.cpp
+++
@@ -1,6 +0,0 @@
-#include "Peso.h"
-
-// Clase para representar el peso de una arista (distancia en metros)
-double Peso::obtenerdistancia() const { 
-    return distancia; 
-}
